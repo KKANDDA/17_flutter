901c6ad17ce25eb8c1c71b55d41d5d634131248a
kkandda <diobobo@naver.com>
2024-12-17 09:22:13 +0900

    리드미 파일 작성

--- chap07_test/lib/main.dart
+++ chap07_test/lib/main.dart
@@ -9,6 +9,14 @@ import 'movie_search/movie_search.dart';
 list.where().contains().toList();
 */
 
+/*
+컴파일 시점, 런타임 시점..
+date now 같은 경우가 좋은 예시가 되겠다.
+
+런타임시 작동하기 때문에, final은 되지만 const는 쓸 수 없다.
+const는 컴파일시 할당해 두어야하기 때문..
+* */
+
 void main () {
   runApp(const MyApp());
 }
